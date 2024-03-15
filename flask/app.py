24379b33043cb6660493f922812130a181e1b8c2
Ivaylo Genchev <48411473+Squidfishrl@users.noreply.github.com>
2024-03-15 11:05:36 +0200

    Add DB, ORM and product endpoints (#2)

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,15 @@
-from flask import render_template
-import connexion
+import config
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("./swagger.yml")
+from flask import render_template
+from dotenv import load_dotenv
+
+app = config.connex_app
+app.add_api(config.basedir / "./swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("./templates/test.html")
+    return render_template("./test.html")
 
 if __name__ == "__main__":
+    load_dotenv()
     app.run(host="0.0.0.0", port=8000)
