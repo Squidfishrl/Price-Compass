d7fd128ba9ee85ec65f29cf2b6e8a092f28eb73f
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2024-03-15 17:53:56 +0200

    Display prices of different stores

--- flask/app.py
+++ flask/app.py
@@ -6,18 +6,39 @@ app = connexion.App(__name__, specification_dir="./")
 app.add_api("./swagger.yml")
 
 # this will be fetched from the backend
-product1_json = '{\
-    "name": "Смайл Гъмис",\
-    "price": 2.08\
-}'
+product1_json = '''{
+    "brand": "STORCK",
+    "name": "Смайл Гъмис",
+    "category": "food",
+    "imageURL": "",
+    "stores": {
+        "Lidl": [
+            {
+                "price": 2.08,
+                "date": "15.03.2024"
+            },
+            {
+                "price": 2.08,
+                "date": "16.03.2024"
+            }
+        ],
+        "Billa": [
+            {            
+                "price": 2.05,
+                "date": "15.03.2024"
+            },
+            {            
+                "price": 1.99,
+                "date": "16.03.2024"
+            }
+        ]
+    }
+}'''
 product1 = json.loads(product1_json);
 
 @app.route("/")
 def home():
-    return render_template("./analytics.html",
-        name=product1["name"],
-        price=product1["price"]
-    )
+    return render_template("./analytics.html", product=product1)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
--- flask/static/main_chart.js
+++ flask/static/main_chart.js
@@ -1,15 +1,75 @@
 const ctx = document.getElementById('chart');
+// console.log(ctx.dataset.prices);
+const stores = JSON.parse(ctx.dataset.stores);
+
+
+let resetZoomButton = document.querySelector('#reset-zoom');
+resetZoomButton.addEventListener('click', () => {
+	Chart.getChart('chart').resetZoom();
+});
+
+
+let select = document.querySelector('#store > select');
+select.addEventListener('change', () => {
+	let curr = select.options[select.selectedIndex].value;
+	// console.log(priceChart.data.datasets[0].data);
+	if (curr !== 'All') {
+		// priceChart.data.datasets[0].data = prices[curr].map(obj => obj.price);
+		priceChart.config.data.datasets = [
+			{
+				label: "Цена",
+				data: stores[curr].map(obj => obj.price),
+				borderWidth: 1,
+				tension: 0.2
+			}
+		]
+	} else {
+		let allDataPoints = Object.values(stores).flatMap(store => store.map(details => details));
+		priceChart.config.data.datasets = [
+			{
+				label: "макс. цена",
+				data: stores['Lidl'].map(obj => obj.price),
+				borderWidth: 1,
+				backgroundColor: 'rgb(200, 0, 0)',
+				borderColor: 'rgb(150, 0, 0)',
+				tension: 0.2
+			},
+			{
+				label: "мин. цена",
+				data: stores['Billa'].map(obj => obj.price),
+				borderWidth: 1,
+				backgroundColor: 'rgb(0, 200, 0)',
+				borderColor: 'rgb(0, 150, 0)',
+				tension: 0.2
+			}
+		]
+	}
+	priceChart.update();
+});
+
 
 let priceChart = new Chart(ctx, {
 	type: 'line',
-		data: {
+	data: {
 		labels: ['14.03', '15.03', '16.03', '17.03', '18.03', '19.03', '20.03', '21.03', '22.03', '23.03', '24.03', '25.03'],
-		datasets: [{
-			label: 'Цена',
-			data: [2.08, 2.08, 2.08, 2, 2, 2.10, 2.05, 2.05, 2.05, 2.11],
-			borderWidth: 1,
-			tension: 0.2
-		}]
+		datasets: [
+			{
+				label: "макс. цена",
+				data: stores['Lidl'].map(obj => obj.price),
+				borderWidth: 1,
+				backgroundColor: 'rgb(200, 0, 0)',
+				borderColor: 'rgb(150, 0, 0)',
+				tension: 0.2
+			},
+			{
+				label: "мин. цена",
+				data: stores['Billa'].map(obj => obj.price),
+				borderWidth: 1,
+				backgroundColor: 'rgb(0, 200, 0)',
+				borderColor: 'rgb(0, 150, 0)',
+				tension: 0.2
+			}
+		]
 	},
 	options: {
 		responsive: true,
@@ -42,7 +102,3 @@ let priceChart = new Chart(ctx, {
 		}
 	}
 });
-
-document.querySelector("#reset-zoom").addEventListener("click", () => {
-	Chart.getChart('chart').resetZoom();
-})
